576af2d39ce4b9f5b9392b9298bc0784e1c3dd75
mwakisaghu <harrisonchambela2016@gmail.com>
2022-11-22 15:23:29 +0300

    This fixes linting indentation issues

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,10 +2,12 @@
 Calculator functions
 """
 
+
 def add(a, b):
-  """Add a and b and return result"""
-  return a + b
+    """Add a and b and return result"""
+    return a + b
+
 
 def subtract(a, b):
-  """Subtract a from b and return a result"""
-  return b - a
+    """Subtrazct a from b and return a result"""
+    return b - a
