4cb6f4f8ad28ed12ac6f420cd8b5017a905cd2f4
mwakisaghu <harrisonchambela2016@gmail.com>
2022-12-16 15:10:51 +0300

    Fixes Indentation

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -18,53 +18,53 @@ INGREDIENTS_URL = reverse('recipe:ingredient-list')
 
 
 def create_user(email='user@example.com', password='testpass123'):
-  """Create and return user"""
-  return get_user_model().objects.create_user(email=email, password=password)
+    """Create and return user"""
+    return get_user_model().objects.create_user(email=email, password=password)
 
 
 class PublicIngredientsApiTests(TestCase):
-  """Test unauthenticated API requests"""
+    """Test unauthenticated API requests"""
 
-  def setUp(self):
-    self.client = APIClient()
+    def setUp(self):
+        self.client = APIClient()
 
-  def test_auth_required(self):
-    """Test auth is required for retrieving ingredients"""
-    res = self.client.get(INGREDIENTS_URL)
+    def test_auth_required(self):
+        """Test auth is required for retrieving ingredients"""
+        res = self.client.get(INGREDIENTS_URL)
 
-    self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
 class PrivateIngredientsApiTest(TestCase):
-  """Test unathenticated API requests"""
+    """Test unathenticated API requests"""
 
-  def setUp(self):
-    self.user = create_user()
-    self.client = APIClient()
-    self.client.force_authenticate(self.user)
+    def setUp(self):
+        self.user = create_user()
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
 
-  def test_retrieve_ingredients(self):
-    """Test retrieving a list of ingredients"""
-    Ingredient.objects.create(user=self.user, name='Kale')
-    Ingredient.objects.create(user=self.user, name='Vanilla')
+    def test_retrieve_ingredients(self):
+        """Test retrieving a list of ingredients"""
+        Ingredient.objects.create(user=self.user, name='Kale')
+        Ingredient.objects.create(user=self.user, name='Vanilla')
 
-    res = self.client.get(INGREDIENTS_URL)
+        res = self.client.get(INGREDIENTS_URL)
 
-    ingredients = Ingredient.objects.all().order_by('-name')
-    serializer = IngredientSerializer(ingredients, many=True)
-    self.assertEqual(res.status_code, status.HTTP_200_OK)
-    self.assertEqual(res.data, serializer.data)
+        ingredients = Ingredient.objects.all().order_by('-name')
+        serializer = IngredientSerializer(ingredients, many=True)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
 
-  def test_ingredients_limited_to_user(self):
-    """Test the list of ingredients is limited to authenticated user"""
-    user2 = create_user(email='user2@example.com')
-    Ingredient.objects.create(user=user2, name='Salt')
-    ingredient = Ingredient.objects.create(user=self.user, name='Pepper')
+    def test_ingredients_limited_to_user(self):
+        """Test the list of ingredients is limited to authenticated user"""
+        user2 = create_user(email='user2@example.com')
+        Ingredient.objects.create(user=user2, name='Salt')
+        ingredient = Ingredient.objects.create(user=self.user, name='Pepper')
 
-    res = self.client.get(INGREDIENTS_URL)
+        res = self.client.get(INGREDIENTS_URL)
 
-    self.assertEqual(res.status_code, status.HTTP_200_OK)
-    self.assertEqual(len(res.data), 1)
-    self.assertEqual(res.data[0]['name'], ingredient.name)
-    self.assertEqual(res.data[0]['id', ingredient.id])
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data[0]['name'], ingredient.name)
+        self.assertEqual(res.data[0]['id', ingredient.id])
 
